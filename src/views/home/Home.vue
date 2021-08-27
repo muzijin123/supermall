452f52040fa4819f89ed086ecc110c70de400bae
muzijin123 <602748532@qq.com>
2021-08-27 11:09:23 +0800

    homePage

---
+++ src/views/home/Home.vue
@@ -0,0 +1,109 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"/>
+    <fearture-view/>
+    <tab-control class="tab-contral" :titles="['流行','新款','精选']" @tabClick="tabClick"/>
+    <goods-list :goods="showGoods" />
+  </div>
+</template>
+
+<script>
+import NavBar from 'components/common/navbar/NavBar.vue'
+import TabControl from 'components/content/tabControl/TabControl.vue'
+
+import {getHomeMultidata} from 'network/home'
+import {getHomeGoods} from 'network/home'
+
+import HomeSwiper from './childComps/HomeSwiper.vue'
+import RecommendView from './childComps/RecommendView.vue'
+import FeartureView from './childComps/FeatureView.vue'
+import GoodsList from 'components/content/goods/GoodsList.vue'
+
+  export default {
+    name: "Home",
+    components: {
+        NavBar,
+        HomeSwiper,
+        RecommendView,
+        FeartureView,
+        TabControl,
+        GoodsList
+    },
+    computed:{
+      showGoods(){
+        return this.goods[this.currentType].list
+      }
+    },
+    data(){
+      return{
+        banners: [],
+        recommends: [],
+        goods:{
+          'pop': {page: 0, list: []},
+          'new': {page: 0, list: []},
+          'sell': {page: 0, list: []}  
+        },
+        currentType:'pop'
+      }
+    },
+    created(){
+      this.getHomeMultidata()
+      this.getHomeGoods('pop')
+      this.getHomeGoods('new')
+      this.getHomeGoods('sell')
+
+    },
+    methods:{
+      tabClick(index){
+        switch(index){
+          case 0:
+            this.currentType = 'pop'
+            break;
+          case 1:
+            this.currentType = 'new'
+            break;
+          case 2:
+            this.currentType = 'sell'
+            break;
+        }
+      },
+
+      getHomeMultidata(){
+        getHomeMultidata().then(res =>{
+          this.banners = res.data.banner.list;
+          this.recommends = res.data.recommend.list;
+        })
+      },
+      getHomeGoods(type){
+        const page = this.goods[type].page + 1
+        getHomeGoods(type,page).then(res =>{
+          this.goods[type].list.push(...res.data.list)
+          this.goods[type].page += 1;
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  #home{
+    padding-top: 44px;
+  }
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top:0;
+    z-index: 9;
+  }
+  .tab-control{
+    position: sticky;
+    top: 44px;
+    z-index: 9;
+  }
+</style>
